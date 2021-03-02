fb969bd7dcb57c20edadbd06f4f9a4a425084b05
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2021-03-02 22:43:25 +0000

    Update WorldFunctions.h

--- WorldFunctions.h
+++ WorldFunctions.h
@@ -1,7 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include "FollowingTextrender.h"
 #include "WorldFunctions.generated.h"
 
 /**
@@ -15,4 +18,8 @@ class EMCGAME_API UWorldFunctions : public UBlueprintFunctionLibrary
 		static void InitBeachLevel(UUserWidget* caller);
 	UFUNCTION(BlueprintCallable, Category = "cpp script")
 		static void InitStreamLevel(UObject* caller,FName Level, TArray<FName> UnloadLevel);
+	UFUNCTION(BlueprintCallable, Category = "item")
+		static TMap<FString, int> WFChangeMoneyValue(TMap<FString, int> OriginalInventory, float MoneyChange);
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		static void SpawnFollwingtextRender(FText TextToSpawn, AActor* Actor, float TimeToLast);
 };
