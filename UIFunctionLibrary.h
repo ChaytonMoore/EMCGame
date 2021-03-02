bba0bf500c286b5b8625fbbe540aa5b30b4a4692
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2021-03-02 22:31:14 +0000

    Update UIFunctionLibrary.h

--- UIFunctionLibrary.h
+++ UIFunctionLibrary.h
@@ -9,6 +9,19 @@
 #include "Engine/DataTable.h"
 #include "UIFunctionLibrary.generated.h"
 
+
+USTRUCT(BlueprintType) //How spells are generated
+struct FSpellMaking : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+			TArray<int> SpellConfiguration;
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+			FName SpellReference;
+
+
+};
+
 UCLASS()
 class EMCGAME_API UUIFunctionLibrary : public UBlueprintFunctionLibrary
 {
@@ -17,6 +30,9 @@ class EMCGAME_API UUIFunctionLibrary : public UBlueprintFunctionLibrary
 		static FString childhoodTextGeneration(int stage, int cultureidx, bool gender, FString choice1, int choice2, int choice3, int choice4, int choice1i);
 	UFUNCTION(BlueprintCallable, Category = "UI Function")
 		static FSkills SkillsInit(int cultureidx, int choice1i, int choice2, int choice3, int choice4);
+	UFUNCTION(BlueprintCallable, Category = "UI Function")
+		static TArray<FString> SpellsInit(int choice3, int choice4);
+
 	UFUNCTION(BlueprintCallable, Category = "UI Function")
 		static FAttribution AttributesInit(int cultureidx, int choice1i, int choice2, int choice3, int choice4, bool gender);
 	
@@ -37,8 +53,30 @@ class EMCGAME_API UUIFunctionLibrary : public UBlueprintFunctionLibrary
 		static void PartiallyRemoveWidget(UUserWidget* Caller);
 
 	UFUNCTION(BlueprintCallable, Category = "UI Function")
-		static FText GetQuestTitle(TMap<class UDataTable*, int> Quests, int idx);
+		static FText GetQuestTitle(TMap<class UDataTable*, int> Quests, int idx, FName zerovalue);
 
 	UFUNCTION(BlueprintCallable, Category = "UI Function")
-		static FText GetQuestDescription(TMap<class UDataTable*, int> Quests, int idx);
+		static FText GetQuestDescription(TMap<class UDataTable*, int> Quests, int idx, FName zerovalue);
+
+	UFUNCTION(BlueprintCallable, Category = "UI Function")
+		static TArray<FString> AddElementToHotBar(TArray<FString> HotBarItems, FString NewElement);
+
+	UFUNCTION(BlueprintCallable, Category = "UI Function", BlueprintPure)
+		static FText GetItemName(class UDataTable* table,FName ItemID);
+	
+	UFUNCTION(BlueprintCallable, Category = "UI Function", BlueprintPure)
+		static FText GetItemAndMagicName(class UDataTable* table, class UDataTable* MagicTable, FName ItemID);
+
+	UFUNCTION(BlueprintCallable, Category = "UI Function", BlueprintPure)
+		static TArray<FString> CompileUseableItems(class UDataTable* table, TMap<FString, int> Items);
+
+	UFUNCTION(BlueprintCallable, Category = "UI Function", BlueprintPure)
+		static bool CheckIfSpell(class UDataTable* table, FString Item);
+
+	UFUNCTION(BlueprintCallable, Category = "Spell Making", BlueprintPure)
+		static FText GenerateSpellRecipe(FSpellMaking SpellToGenerate);
+
+	UFUNCTION(BlueprintCallable, Category = "Spell Making", BlueprintPure)
+		static TArray<FSpellMaking> GenerateSpellConfigurations(class UDataTable* MagicTable);
+
 };
