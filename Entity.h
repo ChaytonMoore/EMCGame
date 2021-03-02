898253bde7d382a1b22ca8b8394e823f7033ab04
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2021-03-02 22:32:08 +0000

    Update Entity.h

--- Entity.h
+++ Entity.h
@@ -3,7 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
 #include "GameFramework/Character.h"
+#include "Perception/PawnSensingComponent.h"
+#include "UObject/ConstructorHelpers.h"
 #include "Entity.generated.h"
 
 
@@ -47,6 +50,20 @@ struct FSpell
 
 };
 
+USTRUCT(BlueprintType) //There is also a version of this in blueprints.
+struct FFactionStruct : public FTableRowBase
+{
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+		FString Name;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+		TArray<FString> EnemyFactions;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+		TArray<FString> AlliedFactions;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+		FString DevDescription;
+};
+
 UCLASS()
 class EMCGAME_API AEntity : public ACharacter
 {
@@ -55,6 +72,9 @@ class EMCGAME_API AEntity : public ACharacter
 public:
 	// Sets default values for this character's properties
 	AEntity();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+		UPawnSensingComponent* SensingComponent;
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
 		float Health;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
@@ -72,6 +92,8 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
 		bool ExposeInventoryOnDeath;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+		bool Dead;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
 		int Level;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
 		int TotalXP;
@@ -84,6 +106,8 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
 		bool Agro;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+		bool activate;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
 		FString TransportString;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
 		FString State = "None";
@@ -91,7 +115,96 @@ public:
 		TArray<FString> EntityTags;
 	UFUNCTION()
 		TMap<FString, int> DefaultMerchantRefillInventory(TMap<FString, int> InitalInventory, TArray<FString> MerchantTags);
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		void RandomShortWalkAround();
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		void RandomLongWalkAround();
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		void EnterDialogueMotion();
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		bool GotoRandomFarmingNode();
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		bool SimpleEntityMoveToLocation(FVector Location);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "NPC")
+		float FindSafetyofPoint(TArray<AEntity*> Enemies, FVector Position);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Utility")
+		bool isEntityMoving();
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "NPC")
+		void BlueprintNPCCombatTick();
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		void NPCCombatTick();
 	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+		FString ActiveSpell;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+		bool inDialogue = true;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+		FName Settlement;
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "NPC")
+		void ExecuteOnDeath();
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		AEntity* FindNearestEntityNotinFaction(float SphereSize);
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		TArray<AEntity*> GetNearByEntities(float SphereSize);
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		TArray<AEntity*> GetEnemiesFromEntites(TArray<AEntity*> Entities);
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		TArray<AEntity*> GetAlliesFromEntites(TArray<AEntity*> Entities);
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		void MonsterProwl(float radius);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+		bool isAttacking = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+		AEntity* AIAttackTarget;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default")
+		TArray<AEntity*> AITargets;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "NPC")
+		bool CalculateFightOrFlight(TArray<AEntity*> Allies, TArray<AEntity*> Enemies);
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		void RunAway();
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		bool LocationNearEntity(FVector Location, float Distance, TArray<AEntity*> Entities);
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default", meta = (AllowPrivateAccess = "true"))
+		float FallDamageValue;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Default", meta = (AllowPrivateAccess = "true"))
+		bool DeathSinking;
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		void AIStateHandling();
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		void AIResponce();
+
+	UFUNCTION(BlueprintCallable, Category = "NPC")
+		bool AgroCheck();
+
+	UPROPERTY(EditAnywhere, Category = "Default")
+		class UDataTable* FactionData;
 
 
 protected:
